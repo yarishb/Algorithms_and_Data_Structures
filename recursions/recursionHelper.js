b7bdcf3051215a27d31b23b9e31540cc8014a640
yarishb <bogdan040105@gmail.com>
2020-08-12 13:56:25 +0300

    added recursion helper function

---
+++ recursions/recursionHelper.js
@@ -0,0 +1,19 @@
+function collectOdds(arr){
+     let result = []
+
+     function helper(inputHelper){
+          if(inputHelper.length === 0){
+               return;
+          }
+          if(inputHelper[0] % 2 === 0){
+               result.push(inputHelper[0])
+          } 
+          helper(inputHelper.slice(1))
+     }
+
+     helper(arr)
+
+     return result
+}
+
+collectOdds([1,2,3,4,5,7,8,9])
