8529a56266a28a8cfa07a7d7dba95132199cddf7
yarishb <bogdan040105@gmail.com>
2020-08-13 11:22:25 +0300

    added build in js sorting algorithms

---
+++ sortingAlgorithms/buildInSort/buildIn.js
@@ -0,0 +1,11 @@
+function numberCompare(num1, num2){
+     return num1 - num2
+}
+
+[6,4,5,7,54,26,14,8].sort(numberCompare)
+
+function compareStrings(str1, str2){
+     return str1.length - str2.length
+}
+
+["Bohdan", "Yarish", "Web Developer", "React", "Js", "Data Structures", "Algorithms"].sort(compareStrings)
