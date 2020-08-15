b6ea9d233fad71f56674c038b1e3a781912e8d55
yarishb <bogdan040105@gmail.com>
2020-08-15 09:29:27 +0300

    added quick sorting algorithm

--- sortingAlgorithms/mergeSort/mergeSort.js
+++ sortingAlgorithms/mergeSort/mergeSort.js
@@ -38,4 +38,4 @@ function merge(arr, arr2){
      return result
 }
 
-mergeSort([1,10,50,40,14,99,100])
+mergeSort(['Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming','California','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota'])
