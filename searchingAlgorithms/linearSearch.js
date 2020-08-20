bf88452ff43b6a507e3ecd52c95038c9419ad747
yarishb <bogdan040105@gmail.com>
2020-08-20 12:43:43 +0300

    added stackes and queues

--- searchingAlgorithms/linearSearch.js
+++
@@ -1,9 +0,0 @@
-function linearSearch(arr, num){
-     //Finding element in array using linear search algorithm
-     for(let j = 0; j <= arr.length; j++){
-          if(arr[j] === num) return j
-     }
-     return -1
-}
-
-linearSearch([1,2,4,5,6,2,3,5], 4)
