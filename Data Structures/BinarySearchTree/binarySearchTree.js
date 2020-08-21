0223323e55f41e18bb37f944214323077230c2e2
yarishb <bogdan040105@gmail.com>
2020-08-21 10:04:18 +0300

    added breadth first search

--- Data Structures/BinarySearchTree/binarySearchTree.js
+++ Data Structures/BinarySearchTree/binarySearchTree.js
@@ -12,42 +12,31 @@ class BinarySearchTree {
      }
 
      insert(value){
-          let newNode = new Node(value)
-          
-          if(this.root === null) {
-               this.root = newNode
-               return this
+          var newNode = new Node(value);
+          if(this.root === null){
+              this.root = newNode;
+              return this;
           }
-          else {
-               let current = this.root
-
-               while(true){
-                    if(current.value === value) return undefined
-
-                    if(!this.root) {
-                         this.root = newNode
-                         return this
-                    }
-                    if(value < current.val){
-                         if(current.left === null){
-                              current.left = newNode
-                              return this
-                         } else {
-                              current = current.left
-                         }
-                    } else if( value > current.val) {
-                         if(current.right === null){
-                              current.right = newNode
-                              return this
-                         } else {
-                              current = current.right
-                         }
-                    }
-               }
+          var current = this.root;
+          while(true){
+              if(value === current.val) return undefined;
+              if(value < current.val){
+                  if(current.left === null){
+                      current.left = newNode;
+                      return this;
+                  }
+                  current = current.left;
+              } else {
+                  if(current.right === null){
+                      current.right = newNode;
+                      return this;
+                  } 
+                  current = current.right;
+              }
           }
-     }
+      }
 
-     find(val){
+     find(value){
           if(this.root === null) return false
           let current = this.root;
           let found = false
@@ -63,13 +52,28 @@ class BinarySearchTree {
           if(!found) return false
           return current
      }
+     bfs(){
+          let node = this.root,
+              data = [],
+              queue = [];
+          
+          queue.push(node)
+
+          while(queue.length){
+               node = queue.shift()
+               data.push(node.val)
+               if(node.left) queue.push(node.left)
+               if(node.right) queue.push(node.right)
+          }
+          return data
+      }
 }
 
 var tree = new BinarySearchTree();
-tree.insert(10)
-tree.insert(5)
-tree.insert(13)
-tree.insert(11)
-tree.insert(2)
-tree.insert(16)
-tree.insert(7)
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+tree.bfs();
