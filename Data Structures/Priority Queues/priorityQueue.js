1702c0925a1a39d6b8c04f8a8b7667fea260c9cd
yarishb <bogdan040105@gmail.com>
2020-08-24 13:07:39 +0300

    Added priority queue

--- Data Structures/Priority Queues/priorityQueue.js
+++ Data Structures/Priority Queues/priorityQueue.js
@@ -1,34 +1,36 @@
-class BinaryHeap{
-     constructor(){
-          this.values = [41,39,33,18,27,12]
+class PriorityQueue{
+     constructor(values) {
+          this.values = []
      }
 
-     insert(value){
-          this.values.push(value)
-
+     enqueue(val, priority){
+          let newNode = new Node(val, priority)
+          this.values.push(newNode)
+          
           let index = this.values.length - 1
           while(index > 0){
                let parentIndex = Math.floor((index - 1) / 2);
-               if(this.values[index] <= this.values[parentIndex]) break
+               if(this.values[index].priority >= this.values[parentIndex].priority) break
                [this.values[parentIndex], this.values[index]] = [this.values[index], this.values[parentIndex]]
                index = parentIndex
           }
      }
-     remove(){
-          let  max = this.values[0],
-               end = this.values.pop()
-          
-          if(this.values.length > 0){
+
+     dequeue(){
+          let  min = this.values[0],
+               end = this.values.pop();
+          if(this.values.length){  
                this.values[0] = end
-               this.sinkDown()
+               this.sinkDown();
           }
-          return max
+          return min
      }
 
      sinkDown(){
           let  index = 0,
                length = this.values.length,
                element = this.values[0]
+          
           while(true){
                let  leftIdx = 2 * index + 1,
                     rightIdx = 2 * index + 2,
@@ -37,20 +39,20 @@ class BinaryHeap{
 
                if(leftIdx < length){
                     left = this.values[leftIdx]
-                    if(left > element){
-                         swap = leftIdx
+                    if(left.priority < element.priority){
+                         swap = left
                     }
                }
                if(rightIdx < length){
                     right = this.values[rightIdx]
                     if(
-                         (swap === null && right > element) ||
-                         (swap !== null && right > left)
-                    ){
-                         swap = rightIdx
+                         (swap === null && right.priority < element.priority) 
+                         || 
+                         (swap !== null && right.priority < left.priority))
+                    {
+                         swap = right
                     }
                }
-
                if(swap === null) break
                this.values[index] = this.values[swap]
                this.values[swap] = element
@@ -59,5 +61,9 @@ class BinaryHeap{
      }
 }
 
-let heap = new BinaryHeap()
-heap.insert(55)
+class Node{
+     constructor(val, priority) {
+          this.val = val,
+          this.priority = priority
+     }
+}
