6208d1f62aa550e153c4a084005a5f9e6621f4c0
yarishb <bogdan040105@gmail.com>
2020-08-25 19:48:25 +0300

    added remove methods for graphs

--- Data Structures/Graphs/graph.js
+++ Data Structures/Graphs/graph.js
@@ -4,7 +4,7 @@ class Graph{
      }
 
      addVertex(name){
-          if(!this.adjacencyList)this.adjacencyList[name] = [] 
+          if(!this.adjacencyList[name])this.adjacencyList[name] = [] 
           return this.adjacencyList
      }
      addEdge(vertex1, vertex2){
@@ -12,4 +12,27 @@ class Graph{
           this.adjacencyList[vertex2].push(vertex1)
           return this.adjacencyList
      }
+     removeEdge(vertex1, vertex2){
+          this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+               v => v !== vertex2
+          )
+          this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+               v1 => v1 !== vertex1 
+          )
+          return this.adjacencyList
+     }
+     removeVertex(name){
+          while(this.adjacencyList[name].length){
+               let adjacentVertex = this.adjacencyList[name].pop()
+               this.removeEdge(name, adjacentVertex)
+          }
+          delete this.adjacencyList[name]
+     }
 }
+
+let g = new Graph()
+g.addVertex("Dallas");
+g.addVertex("Tokyo");
+g.addVertex("Aspen");
+g.addEdge("Dallas", "Tokyo");
+g.addEdge("Dallas", "Aspen");
