56dd1db997ec824693fb01b53b0c44211166e0ba
yarishb <bogdan040105@gmail.com>
2020-08-26 15:52:27 +0300

    added bfs and dfs for graphs

--- Data Structures/Graphs/graph.js
+++ Data Structures/Graphs/graph.js
@@ -28,11 +28,82 @@ class Graph{
           }
           delete this.adjacencyList[name]
      }
+     dfsRecursive(vertex){
+          let result = [];
+          let visited = {};
+          const adjacencyList = this.adjacencyList
+
+          function dfs(vertex){
+               if(!vertex) return null;
+               visited[vertex] = true;
+               result.push(vertex);
+
+               adjacencyList[vertex].forEach(neighbor => {
+                    if(!visited[neighbor]){
+                       return dfs(neighbor)
+                    }
+               });
+          }
+          dfs(vertex)
+          return result
+     }
+     dfsIterative(vertex){
+          let  stack = [vertex],
+               result = [],
+               visited = {},
+               temp;
+          visited[vertex] = true
+
+          while (stack.length){
+               temp = stack.pop();
+               result.push(temp)
+               this.adjacencyList[temp].forEach(neighbor =>{
+                    if(!visited[neighbor]){
+                         visited[neighbor] = true
+                         stack.push(neighbor)
+                    }
+               })
+          }
+
+         return result
+     }
+     breadthFirstSearch(vertex){
+          let  queue = [vertex],
+               result = [],
+               visited = {},
+               temp;
+
+          visited[vertex] = true
+          
+          while(queue.length){
+               temp = queue.shift()
+               result.push(temp)
+               this.adjacencyList[temp].forEach(neighbor => {
+                    if(!visited[neighbor]){
+                         visited[neighbor] = true;
+                         queue.push(neighbor)
+                    }
+               })
+               
+          }
+          
+
+          return result
+     }
 }
 
 let g = new Graph()
-g.addVertex("Dallas");
-g.addVertex("Tokyo");
-g.addVertex("Aspen");
-g.addEdge("Dallas", "Tokyo");
-g.addEdge("Dallas", "Aspen");
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+
