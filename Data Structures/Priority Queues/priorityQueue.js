5b2ec03e5d85125ca7404f3e137864de11d4280f
yarishb <bogdan040105@gmail.com>
2020-08-27 14:52:14 +0300

    fixed priority queue

--- Data Structures/Priority Queues/priorityQueue.js
+++ Data Structures/Priority Queues/priorityQueue.js
@@ -1,69 +1,70 @@
-class PriorityQueue{
-     constructor(values) {
-          this.values = []
-     }
 
+class PriorityQueue {
+     constructor(){
+         this.values = [];
+     }
      enqueue(val, priority){
-          let newNode = new Node(val, priority)
-          this.values.push(newNode)
-          
-          let index = this.values.length - 1
-          while(index > 0){
-               let parentIndex = Math.floor((index - 1) / 2);
-               if(this.values[index].priority >= this.values[parentIndex].priority) break
-               [this.values[parentIndex], this.values[index]] = [this.values[index], this.values[parentIndex]]
-               index = parentIndex
-          }
+         let newNode = new Node(val, priority);
+         this.values.push(newNode);
+         this.bubbleUp();
      }
-
+     bubbleUp(){
+         let idx = this.values.length - 1;
+         const element = this.values[idx];
+         while(idx > 0){
+             let parentIdx = Math.floor((idx - 1)/2);
+             let parent = this.values[parentIdx];
+             if(element.priority >= parent.priority) break;
+             this.values[parentIdx] = element;
+             this.values[idx] = parent;
+             idx = parentIdx;
+         }
+     }
      dequeue(){
-          let  min = this.values[0],
-               end = this.values.pop();
-          if(this.values.length){  
-               this.values[0] = end
-               this.sinkDown();
-          }
-          return min
+         const min = this.values[0];
+         const end = this.values.pop();
+         if(this.values.length > 0){
+             this.values[0] = end;
+             this.sinkDown();
+         }
+         return min;
      }
-
      sinkDown(){
-          let  index = 0,
-               length = this.values.length,
-               element = this.values[0]
-          
-          while(true){
-               let  leftIdx = 2 * index + 1,
-                    rightIdx = 2 * index + 2,
-                    swap = null,
-                    left,right;
-
-               if(leftIdx < length){
-                    left = this.values[leftIdx]
-                    if(left.priority < element.priority){
-                         swap = left
-                    }
-               }
-               if(rightIdx < length){
-                    right = this.values[rightIdx]
-                    if(
-                         (swap === null && right.priority < element.priority) 
-                         || 
-                         (swap !== null && right.priority < left.priority))
-                    {
-                         swap = right
-                    }
-               }
-               if(swap === null) break
-               this.values[index] = this.values[swap]
-               this.values[swap] = element
-               index = swap
-          }
+         let idx = 0;
+         const length = this.values.length;
+         const element = this.values[0];
+         while(true){
+             let leftChildIdx = 2 * idx + 1;
+             let rightChildIdx = 2 * idx + 2;
+             let leftChild,rightChild;
+             let swap = null;
+ 
+             if(leftChildIdx < length){
+                 leftChild = this.values[leftChildIdx];
+                 if(leftChild.priority < element.priority) {
+                     swap = leftChildIdx;
+                 }
+             }
+             if(rightChildIdx < length){
+                 rightChild = this.values[rightChildIdx];
+                 if(
+                     (swap === null && rightChild.priority < element.priority) || 
+                     (swap !== null && rightChild.priority < leftChild.priority)
+                 ) {
+                    swap = rightChildIdx;
+                 }
+             }
+             if(swap === null) break;
+             this.values[idx] = this.values[swap];
+             this.values[swap] = element;
+             idx = swap;
+         }
      }
-}
-
-class Node{
-     constructor(val, priority) {
-          this.val = val,
-          this.priority = priority
+ }
+ 
+ class Node {
+     constructor(val, priority){
+         this.val = val;
+         this.priority = priority;
      }
-}
+ }
